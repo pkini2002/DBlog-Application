0aa634c00eb6e84cc5dd9ac342680604e8e2b939
PrathikshaKini <kiniprathiksha18@gmail.com>
2022-12-04 12:56:03 +0530

    added category list

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,19 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    cats=Category.objects.all()
     ordering = ['-id']
+
+    def get_context_data(self,*args,**kwargs):
+        cat_menu=Category.objects.all()
+        context=super(HomeView,self).get_context_data(*args,**kwargs)
+        context["cat_menu"]=cat_menu
+        return context
+
+def CategoryListView(request):
+    cat_menu_list=Category.objects.all()
+    return render(request,'category_list.html',{'cat_menu_list':cat_menu_list})
+
 
 def CategoryView(request,cats):
     category_posts=Post.objects.filter(category=cats.replace('-',' '))
@@ -17,6 +29,12 @@ def CategoryView(request,cats):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self,*args,**kwargs):
+        cat_menu=Category.objects.all()
+        context=super(ArticleDetailView,self).get_context_data(*args,**kwargs)
+        context["cat_menu"]=cat_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post
