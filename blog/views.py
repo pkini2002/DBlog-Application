12253c5e54261a2f0ca7ab57ed2ec380b5fb1de9
PrathikshaKini <kiniprathiksha18@gmail.com>
2022-12-04 11:06:13 +0530

    changes

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,10 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-id']
 
+def CategoryView(request,cats):
+    category_posts=Post.objects.filter(category=cats)
+    return render(request,'categories.html',{'cats':cats,'category_posts':category_posts})
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
