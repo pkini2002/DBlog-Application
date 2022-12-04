c2a6c7cc390274af1bff163aad7d5d6abb027ed9
Palguni <palsamly3000@gmail.com>
2022-12-04 18:46:44 +0530

    auth-complete

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import date,datetime
+from ckeditor.fields import RichTextField
 import django.utils.timezone
 
 # Create your models here.
@@ -18,9 +19,11 @@ class Post(models.Model):
     title=models.CharField(max_length=255)
     title_tag=models.CharField(max_length=255,default="")
     author=models.ForeignKey(User,on_delete=models.CASCADE)
-    body=models.TextField()
+    body=RichTextField(blank=True,null=True)
+    #body=models.TextField()
     post_date=models.DateField(auto_now_add=True)
     category=models.CharField(max_length=255,default='coding')
+    snippet=models.CharField(max_length=255)
 
     def __str__(self):
         return self.title + " | " + str(self.author)
